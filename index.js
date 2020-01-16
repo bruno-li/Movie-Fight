fab54def595629a08faa9c9951c09d17bce49c5f
Bruno Li <lisiuloong@gmail.com>
2020-01-16 08:36:38 -0500

    set up helper ffuction to fetch data from API

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hi there!');
+//helper function to fetch data from API using Axios
+const fetchData = async () => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		// use axios to pass an object parameters for the api
+		params: {
+			apiKey: '7cab3882',
+			//property, from the API documentation to look up a movie
+			i: 'tt0848228'
+		}
+	});
+	console.log(response.data);
+};
+fetchData();
